1816585646507112ac387e8e485036d8a5dd9bf3
gus-gouvea <gustavo.gouvea@outlook.com>
2024-09-20 11:06:39 -0300

    Gusssss

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'World do Gusss')
+    target = os.environ.get('TARGET', 'World do Gussss')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
